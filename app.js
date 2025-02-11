0f777feec6450d3037a473dd925d24d31512e1b3
Kami <hppavilion120000@gmail.com>
2025-02-11 15:04:37 +0500

    successfully getting all games first moves as white and black.

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express');
+const pgnParser = require('pgn-parser');
 const cors = require('cors');
-
 const mongoose = require('mongoose');
+const e = require('express');
 mongoose.connect('mongodb://127.0.0.1:27017/chessLikelyMoveExtensionDb').then(() => {
     console.log("Mongoose Server Started!");
 }).catch((err) => {
     console.log("Err mongoose!");
 });
+
 const app  = express();
 app.use(cors()); 
 app.use(express.json()); 
@@ -25,15 +27,29 @@ app.post('/api/opponent', async(req, res) => {
     console.log('Received Opponent Name:', latestOpponentName);
     try {
         // Fetch player information from Chess.com API
-        const response = await fetch(`https://api.chess.com/pub/player/${latestOpponentName}`);
+        const response = await fetch(`https://api.chess.com/pub/player/${latestOpponentName}/games/live/900/10`);
         if (!response.ok) {
             return res.status(response.status).json({ error: 'Error fetching player data from Chess.com' });
         }
         const playerData = await response.json();
-        console.log('Player Data:', playerData);
+        // console.log('Player Data:', playerData);
+        const filteredGames = playerData.games.filter(game =>game.time_control=== "900+10" && game.black.username.toLowerCase() === latestOpponentName.toLowerCase());
+        // const top100Games = filteredGames.slice(0,100);
+        // console.log(filteredGames);
+        let everyGameFirstTwoMoves = [];
+        filteredGames.forEach(eachGame => {
+            const [resultofpgnparse] = pgnParser.parse(eachGame.pgn);
+            let eachGameFirstTwoMoves = {
+                firstMoveWhite: resultofpgnparse.moves[0].move,
+                firstMoveBlack: resultofpgnparse.moves[1].move
+            };
+            everyGameFirstTwoMoves.push(eachGameFirstTwoMoves);
+        });
 
+        console.log(everyGameFirstTwoMoves);
+
+        res.json({ success: true, filteredGames });
         // Send the player data back in the response
-        res.json({ success: true, playerData });
     } catch (error) {
         console.error('Error:', error);
         res.status(500).json({ error: 'Server error' });
